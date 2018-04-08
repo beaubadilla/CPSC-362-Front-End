ad83cb67d77ac84338bbb36c51e686d25de85d5d
beauJB <beau.badilla@csu.fullerton.edu>
2018-04-07 22:13:50 -0700

    Basic data retrieval from backend to frontend

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!--<img src="./assets/logo.png">-->
     <login></login>
     <!--<router-view/>-->
   </div>
 </template>
 
 <script>
- import Login from './components/login'
+import Login from './components/login'
 export default {
   name: 'App',
   components: {
-     Login
+    Login
   }
 }
 </script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,51 +1,67 @@
-
 <template>
 <div>
-	<div class="login">
-		<h1 v-text="title"></h1>
-	</div>
-	<div>
-		<form>
-			<p v-text="emailTitle"></p>
-			<input type="text" v-model="email" v-on:keyup.enter="loginAttempt">
-			<p v-text="passwordTitle"></p>
-			<input type="text" v-model="password" v-on:keyup.enter="loginAttempt">
-			<p v-text="forgotPasswordTitle"></p>
-			<p v-text="alreadyMemberTitle"></p>
-			<button v-on:click="loginAttempt">Login</button>
-		</form>
-	</div>
+  <div class="login">
+    <h1 v-text="title"></h1>
+  </div>
+  <div>
+    <form class="login-box">
+    <p v-text="emailTitle"></p>
+      <input type="text" v-model="email" v-on:keyup.enter="loginAttempt">
+      <p v-text="passwordTitle"></p>
+      <input type="text" v-model="password" v-on:keyup.enter="loginAttempt">
+      <p v-text="forgotPasswordTitle"></p>
+      <p v-text="alreadyMemberTitle"></p>
+      <button v-on:click="loginAttempt">Login</button>
+    </form>
+  </div>
 </div>
 </template>
 
-</template>
-
 <script>
-	export default {
-		name: 'login',
-		data() {
-			return {
-				title: 'Login',
-				emailTitle: 'Email',
-				passwordTitle: 'Password',
-				forgotPasswordTitle: 'Forgot Password?',
-				alreadyMemberTitle: 'Already a member?',
-				email:'',
-				password:''
-			}
-		},
-		methods: {
-			loginAttempt: function() {
-				//request?
-			}
-		},
-		mounted () {
-			this.axios.get('https://jsonplaceholder.typicode.com/posts/1').then((response) => {
-				console.log(response)
-			})
-		}
-	}
+export default {
+  name: 'login',
+  data () {
+    return {
+      title: 'Login',
+      emailTitle: 'Email',
+      passwordTitle: 'Password',
+      forgotPasswordTitle: 'Forgot Password?',
+      alreadyMemberTitle: 'Already a member?',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    loginAttempt: function () {
+      // request?
+    }
+  },
+  mounted () {
+    this.axios.get('https://jsonplaceholder.typicode.com/posts/1').then((response) => {
+      console.log(response)
+    })
+  }
+}
 </script>
 
 <style scoped>
+.login{
+  margin-top: 225px;          /* Probably not the best way to do the margins, but it works for now...*/
+  text-align: center;
+  font-size: 36px;
+  font-family: sans-serif;
+  color: orange;
+}
+.login-box{
+    width: 500px;
+    height: 380px;
+    background: rgba(0, 0, 0, 0.5);      /* this setting makes the login box transparent */
+    color: white;
+    top: 55%;                            /* Centers the login box */
+    left: 50%;                           /* Centers the login box */
+    transform: translate(-50%,-50%);     /* Centers the login box */
+    position: absolute;
+    box-sizing: border-box;
+    padding: 30px 20px;
+}
 </style>
