36b1a5e54072cb72b79925981dde0560d5122fba
beauJB <beau.badilla@csu.fullerton.edu>
2018-04-07 22:58:22 -0700

    Returned back to Jonathan's working login
    Should be exactly what Jonathan fixed from my original prototype. Having commit/pull/pushing errors and this seems like the easiest way to fix it.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,11 +4,7 @@
     <h1 v-text="title"></h1>
   </div>
   <div>
-<<<<<<< HEAD
-    <form class="login-box">
-=======
     <form>
->>>>>>> b8437ac6e1faa54b049bb341882fac76aa4c2c27
     <p v-text="emailTitle"></p>
       <input type="text" v-model="email" v-on:keyup.enter="loginAttempt">
       <p v-text="passwordTitle"></p>
@@ -49,26 +45,4 @@ export default {
 </script>
 
 <style scoped>
-<<<<<<< HEAD
-.login{
-  margin-top: 225px;          /* Probably not the best way to do the margins, but it works for now...*/
-  text-align: center;
-  font-size: 36px;
-  font-family: sans-serif;
-  color: orange;
-}
-.login-box{
-    width: 500px;
-    height: 380px;
-    background: rgba(0, 0, 0, 0.5);      /* this setting makes the login box transparent */
-    color: white;
-    top: 55%;                            /* Centers the login box */
-    left: 50%;                           /* Centers the login box */
-    transform: translate(-50%,-50%);     /* Centers the login box */
-    position: absolute;
-    box-sizing: border-box;
-    padding: 30px 20px;
-}
-=======
->>>>>>> b8437ac6e1faa54b049bb341882fac76aa4c2c27
 </style>
