3fa7509f5a0f9ce8b569cdb7215090f4330f9764
wbui3 <buiquangwilliam@yahoo.com>
2018-04-28 15:05:42 -0700

    added routing to index.js

--- src/components/NewCategory.vue
+++ src/components/NewCategory.vue
@@ -1,13 +1,14 @@
 /* eslint-disable */
 <template>
   <div>
-    <v-btn v-on:click="addCategory" color="orange lighten-1" dark>Create</v-btn>
+    <v-btn v-on:click="addCategory" color="orange lighting-1" dark>Create</v-btn>
     <v-text-field v-model="categoryName"></v-text-field >
   </div>
 </template>
 
 <script>
 import Toolbar from './Toolbar'
+import index from '../router/index'
 export default {
   data () {
     return {
@@ -15,11 +16,13 @@ export default {
     }
   },
   components: {
-    'Toolbar': Toolbar
+    'Toolbar': Toolbar,
+    'index': index
   },
   method: {
     addCategory: function () {
       Toolbar.items.children.push({href: '/$categoryName', text: 'categoryName'})
+      index.routes.children.push({path: '/$categoryName', name: '/$categoryName', component: '/$categoryName'})
       console.log('I added a new category ')
     }
   }
