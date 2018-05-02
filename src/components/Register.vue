1b49462714b0cc99aaa5116aa4d94162ed753fef
Syaora <saper209@yahoo.com>
2018-05-01 19:49:21 -0700

    Accidentally deleted earlier

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="app">
   <v-app id="inspire">
-    <v-content>
+    <v-content id="background">
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md4>
