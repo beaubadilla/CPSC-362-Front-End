6650c33165c73120c9868d0155bbcce9ffaf4634
saper209@yahoo.com <saper209@yahoo.com>
2018-05-02 01:02:36 -0700

    Updated template for Search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,20 +1,87 @@
 <template>
-<div>
+  <v-container fluid grid-list-lg>
     <ais-index
         app-id='46REFXDXXW'
         api-key='053b6f707307dc7985393b5c0eec6cbd'
         index-name='notes'>
-
-        <ais-input placeholder="Search notes..." id="SearchBar"></ais-input>
-           <ais-results>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <h2> Search </h2>
+          </v-flex>
+          <v-flex class="searchBar">
+            <ais-search-box placeholder="Search notes..." id="SearchBar"></ais-search-box>
+          </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <ais-results>
               <template slot-scope="{ result }">
-                <div>
-                  <ul>
-                    <li>{{result.title}}</li>
-                  </ul>
-                </div>
+                <v-flex xs12>
+                  <v-card>
+                    <v-card-row class="blue-grey darken-1">
+                      <v-card-title primary-title>
+                        <h4> {{ result.title }} </h4>
+                      </v-card-title>
+                    </v-card-row>
+                    <v-card-text>
+                      <p>
+                        {{ result.description }}
+                      </p>
+                    </v-card-text>
+                    <v-card-row actions class="blue-grey darken-1">
+                      <router-link :to="{ name: 'user', params: {userId: 123}}"><v-btn flat class="white--text">View Notes</v-btn></router-link>
+                    </v-card-row>
+                  </v-card>
+                </v-flex>
               </template>
-           </ais-results>
+            </ais-results>
+          </v-flex>
+        </v-layout>
     </ais-index>
-</div>
+  </v-container>
 </template>
+
+<style>
+.input-group {
+  height: 100px;
+}
+
+.searchBar {
+  position: relative;
+}
+
+.ais-input {
+  width: 100%;
+  outline: none;
+  font-size: 15px;
+  padding: 7px;
+  box-sizing: border-box;
+  border: 2px solid lightgrey;
+  border-radius: 2px;
+  margin: 20px 0;
+  padding-left: 40px;
+  margin-right: 5%;
+}
+
+.ais-search-box__submit,
+.ais-clear {
+  background: none;
+  border: none;
+  position: absolute;
+  z-index: 30;
+  top: 40px;
+}
+
+.ais-search-box__submit {
+  left: 20px;
+}
+
+.ais-clear {
+  right: 20px;
+}
+
+.ais-clear--disabled {
+  display: none;
+}
+
+</style>
